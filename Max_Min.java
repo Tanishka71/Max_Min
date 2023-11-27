46a8181b6808403d7e700430a5eb92908cb61f1b
Tanishka Jain <tanishkajain1508@gmail.com>
2023-11-27 18:42:37 +0530

    ADDED:UC_3_FIND_MAX_TEST_CASES_STRING_VALUE

--- Max_Min.java
+++ Max_Min.java
@@ -1,29 +1,40 @@
 package com.bridgelabz.max;
 
+/*@desc:generic class using data type T*/
 public class Max_Min<T extends Comparable> {
 	
-	public T findMax(T num1,T num2,T num3) {
-		T max= num1.compareTo(num2)>0 ?num1:num2;
-		max=max.compareTo(num3)>0 ? max:num3;
+	/*
+	 * @desc:compares three parameters and gives the max out of the three
+	 * @param:T type
+	 * @return:T type
+	 */
+	public T findMax(T str1,T str2,T str3) {
+		T max= str1.compareTo(str2)>0 ?str1:str2;
+		max=max.compareTo(str3)>0 ? max:str3;
 		return max;
 	}
+	
+	/*
+	 * @desc:main function which has the designed test cases1,2,3
+	 * @param:none
+	 * @return:none
+	 */
 	 public static void main(String[] args) {
 		 
-		 //Test Case 1 where first number is max
-		 Max_Min<Float> ob1=new Max_Min<>();
-		 Float result1=ob1.findMax(12.2f,6.5f,4.4f);
+		   // Test Case 1: Max at 1st position
+	       Max_Min<String> finder1 = new Max_Min<>();
+	        String result1 = finder1.findMax("Apple", "Peach", "Banana");
 	        System.out.println("Test Case 1 Result: " + result1);
-		 
-		 //Test Case 2 where second number is max
-		 Max_Min<Float> ob2=new Max_Min<>();
-		 Float result2=ob2.findMax(1.1f,6.3f,4.9f);
+
+	        // Test Case 2: Max at 2nd position
+	       Max_Min<String> finder2 = new Max_Min<>();
+	        String result2 = finder2.findMax("Orange", "Grapes", "Kiwi");
 	        System.out.println("Test Case 2 Result: " + result2);
-		 
-		 //Test Case 3 where second number is max
-		 Max_Min<Float> ob3=new Max_Min<>();
-		 Float result3=ob3.findMax(1.3f,6.4f,14.5f);
+
+	        // Test Case 3: Max at 3rd position
+	       Max_Min<String> finder3 = new Max_Min<>();
+	        String result3 = finder3.findMax("Cherry", "Strawberry", "Blueberry");
 	        System.out.println("Test Case 3 Result: " + result3);
-	 }
-	
+	    }
 
 }
