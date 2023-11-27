ac62fd5b6e65bd0f2c2f946c48ef9bd2238cc202
Tanishka Jain <tanishkajain1508@gmail.com>
2023-11-27 18:32:35 +0530

    ADDED:UC_1_FIND_MAX_TEST_CASES_FLOAT_VALUES

--- Max_Min.java
+++ Max_Min.java
@@ -10,18 +10,18 @@ public class Max_Min<T extends Comparable> {
 	 public static void main(String[] args) {
 		 
 		 //Test Case 1 where first number is max
-		 Max_Min<Integer> ob1=new Max_Min<>();
-		 Integer result1=ob1.findMax(12,6,4);
+		 Max_Min<Float> ob1=new Max_Min<>();
+		 Float result1=ob1.findMax(12.2f,6.5f,4.4f);
 	        System.out.println("Test Case 1 Result: " + result1);
 		 
 		 //Test Case 2 where second number is max
-		 Max_Min<Integer> ob2=new Max_Min<>();
-		 Integer result2=ob2.findMax(1,6,4);
+		 Max_Min<Float> ob2=new Max_Min<>();
+		 Float result2=ob2.findMax(1.1f,6.3f,4.9f);
 	        System.out.println("Test Case 2 Result: " + result2);
 		 
 		 //Test Case 3 where second number is max
-		 Max_Min<Integer> ob3=new Max_Min<>();
-		 Integer result3=ob3.findMax(1,6,14);
+		 Max_Min<Float> ob3=new Max_Min<>();
+		 Float result3=ob3.findMax(1.3f,6.4f,14.5f);
 	        System.out.println("Test Case 3 Result: " + result3);
 	 }
 	
