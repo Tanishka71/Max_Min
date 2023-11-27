f0d8cd032750dc233559f6bf5c26cb23c4dbb1ae
Tanishka Jain <tanishkajain1508@gmail.com>
2023-11-27 19:02:32 +0530

    ADDED:(UC_4_FIND_MAX_AND_REFACTOR_ALL_THREE_TOGETHER

--- Max_Min.java
+++ Max_Min.java
@@ -4,13 +4,13 @@ package com.bridgelabz.max;
 public class Max_Min<T extends Comparable> {
 	
 	/*
-	 * @desc:compares three parameters and gives the max out of the three
+	 * @desc:compares three objects and gives the max out of the three
 	 * @param:T type
 	 * @return:T type
 	 */
-	public T findMax(T str1,T str2,T str3) {
-		T max= str1.compareTo(str2)>0 ?str1:str2;
-		max=max.compareTo(str3)>0 ? max:str3;
+	public T findMax(T obj1,T obj2,T obj3) {
+		T max= obj1.compareTo(obj2)>0 ?obj1:obj2;
+		max=max.compareTo(obj3)>0 ? max:obj3;
 		return max;
 	}
 	
@@ -27,13 +27,13 @@ public class Max_Min<T extends Comparable> {
 	        System.out.println("Test Case 1 Result: " + result1);
 
 	        // Test Case 2: Max at 2nd position
-	       Max_Min<String> finder2 = new Max_Min<>();
-	        String result2 = finder2.findMax("Orange", "Grapes", "Kiwi");
+	       Max_Min<Float> finder2 = new Max_Min<>();
+	        Float result2 = finder2.findMax(12.3f, 24.3f, 1.8f);
 	        System.out.println("Test Case 2 Result: " + result2);
 
 	        // Test Case 3: Max at 3rd position
-	       Max_Min<String> finder3 = new Max_Min<>();
-	        String result3 = finder3.findMax("Cherry", "Strawberry", "Blueberry");
+	       Max_Min<Integer> finder3 = new Max_Min<>();
+	        Integer result3 = finder3.findMax(1, 2 ,3);
 	        System.out.println("Test Case 3 Result: " + result3);
 	    }
 
